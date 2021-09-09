df60ebf96fcb7b5f6c849e46c02d85799451e964
mex9991 <mex9991@24046-5640.INFO>
2021-09-09 17:32:02 -0300

    criando service com grpc

---
+++ grpc-intro/src/main/java/server/AccountDatabase.java
@@ -0,0 +1,25 @@
+package server;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class AccountDatabase {
+
+    public static final Map<Integer,Integer> MAP = IntStream.rangeClosed(1,10)
+            .boxed()
+            .collect(Collectors.toMap(Function.identity(), v -> v *10));
+
+    public static final int getBalance(int accountId){
+        return MAP.get(accountId);
+    }
+
+    public static int addBalance(int accountId, int ammount) {
+        return MAP.computeIfPresent(accountId,(v,k) -> v+ ammount);
+    }
+
+    public static int deductBalance(int accountId, int ammount) {
+        return MAP.computeIfPresent(accountId,(v,k) -> v - ammount);
+    }
+}
---
+++ grpc-intro/src/main/java/server/GrpcServer.java
@@ -0,0 +1,20 @@
+package server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GrpcServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Server server = ServerBuilder.forPort(6565)
+                .addService(new BankService())
+                .build();
+        server.start();
+        System.out.println("###########################################################################\n" +
+                            "      ##############   gRpc Server : port 6565       ################");
+        server.awaitTermination();
+    }
+}
